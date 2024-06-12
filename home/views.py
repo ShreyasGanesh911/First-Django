caf5ec007cb83daf9241385a2f9734b837f4612e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-12 08:50:25 +0530

    code setup

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def home(request) :
-    return render(request,'index.html')
+    return render(request,'./home/home.html')
     # return HttpResponse("Hello")
